ff1fe133d4833869ee059d62f4dc0aa5df6fa8a3
MartH76 <martin25.husser@gmail.com>
2022-10-17 08:25:29 +0200

    correctif erreur

---
+++ tb.vhd
@@ -0,0 +1,50 @@
+library IEEE;
+use IEEE.Std_logic_1164.all;
+use IEEE.Numeric_Std.all;
+
+library work;
+use work.tp_pkg.all;
+
+entity top_tb is
+end;
+
+architecture bench of top_tb is
+
+  component top
+      port (
+          clk : in std_logic;
+          reset : in std_logic;
+          input_data : in std_logic_vector(WIDTH_OF_RAM-1 downto 0);
+          done : out std_logic;
+          enable_load_ram_i : in std_logic_vector(NBR_TILES-1 downto 0);
+          Datout_o : out std_logic_vector(WIDTH_OF_RAM + WIDTH_OF_ROM + WIDTH_OF_ROM + NBR_TILES downto 0);
+          select_out : in std_logic_vector(NBR_TILES-1 downto 0)
+      );
+  end component;
+
+  signal clk: std_logic := '0';
+  signal reset: std_logic;
+  signal input_data: std_logic_vector(WIDTH_OF_RAM-1 downto 0);
+  signal done: std_logic;
+  signal enable_load_ram_i: std_logic_vector(NBR_TILES-1 downto 0);
+  signal Datout_o: std_logic_vector(WIDTH_OF_RAM + WIDTH_OF_ROM + WIDTH_OF_ROM + NBR_TILES downto 0);
+  signal select_out: std_logic_vector(NBR_TILES-1 downto 0) ;
+
+begin
+
+  uut: top port map ( clk               => clk,
+                      reset             => reset,
+                      input_data        => input_data,
+                      done              => done,
+                      enable_load_ram_i => enable_load_ram_i,
+                      Datout_o          => Datout_o,
+                      select_out        => select_out );
+
+
+    clk <= not clk after 10 ns;
+    reset <= '1' after 100 ns;
+    reset <= '0' after 200 ns;
+    
+    
+
+end;
