1aa21505ae2e49b862f9b3264bd1c73d32b93a49
MartH76 <martin25.husser@gmail.com>
2022-10-17 11:38:20 +0200

    test avec 2 tuiles

--- tp_pkg.vhd
+++ tp_pkg.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 package tp_pkg is
-    constant NBR_TILES : integer range 0 to 100 := 20; 
+    constant NBR_TILES : integer range 0 to 100 := 2; 
     
      
     constant WIDTH_OF_RAM : integer range 0 to 100 := 12;
