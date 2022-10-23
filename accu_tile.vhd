ec118f4abeb2d02df9b04aa0f958378dd513e641
MartH76 <martin25.husser@gmail.com>
2022-10-23 09:24:44 +0200

    correctif sur l'accumulateur

--- accu_tile.vhd
+++ accu_tile.vhd
@@ -23,7 +23,7 @@ end accu_tile;
 
 architecture rtl of accu_tile is
 
-    signal counter_accu_tile : integer range 0 to WIDTH_OF_ROM - 1 := 0;
+    signal counter_accu_tile : integer range 0 to WIDTH_OF_ROM := 0;
 	 
 	signal buff_data_out : std_logic_vector(WIDTH_OF_RAM + WIDTH_OF_ROM + WIDTH_OF_ROM - 1 downto 0) := (others => '0');
 begin
@@ -36,11 +36,11 @@ begin
             elsif rising_edge(clk) then
                 if(start = '1' and select_out = '1') then
 
-                    if (counter_accu_tile = 0) then
-						buff_data_out <= (others => '0');
-                    end if;
+                    --if (counter_accu_tile = 0) then
+					--	buff_data_out <= (others => '0');
+                    --end if;
 
-                    if counter_accu_tile = WIDTH_OF_ROM - 1 then
+                    if counter_accu_tile = WIDTH_OF_ROM then
                         done <= '1';
                         counter_accu_tile <= 0;
                         buff_data_out <= (others => '0');
